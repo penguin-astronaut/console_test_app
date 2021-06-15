19b7e97f0df30bfe53e1eaf62e712d0a22e78fb7
Vadim Kasatkin <penguin2804@gmail.com>
2021-06-15 23:42:39 +0400

    Commands add use convert

--- src/Commands.php
+++ src/Commands.php
@@ -9,6 +9,7 @@ class Commands
     public function scan()
     {
         $config = json_decode(file_get_contents(__DIR__ . '/../config.json'), true);
-        $files = Helpers::getFiles(__DIR__ . '/../' . $config['folder']);
+        $files = Helpers::getFiles(__DIR__ . '/../' . $config['folder'], $config['ext']);
+        Helpers::convertToPdf($files);
     }
 }
