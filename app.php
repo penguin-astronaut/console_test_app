1dc78d233451f4b1fa9bf8318e8bff1f175813ef
Vadim Kasatkin <penguin2804@gmail.com>
2021-06-15 20:49:54 +0400

    app init

--- app.php
+++ app.php
@@ -1,3 +1,9 @@
 <?php
 require 'vendor/autoload.php';
 
+try {
+    $app = new Penguin\ConsoleApp\App();
+    $app->run();
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+}
--- src/App.php
+++ src/App.php
@@ -6,5 +6,20 @@ namespace Penguin\ConsoleApp;
 
 class App
 {
+    public string $commandName;
 
+    public function __construct()
+    {
+        $this->commandName = $_SERVER['argv'][1] ?? 'scan';
+    }
+
+    public function run(): void
+    {
+        $commands = new Commands();
+        if (!method_exists($commands, $this->commandName)) {
+            throw new \Exception('Command not found');
+        }
+
+        call_user_func([$commands,$this->commandName]);
+    }
 }
