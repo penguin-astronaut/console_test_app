859fee86dcc0d36e3ebec70cead69d633d26ff63
Vadim Kasatkin <penguin2804@gmail.com>
2021-06-15 20:50:03 +0400

    Commands.php

--- src/Commands.php
+++ src/Commands.php
@@ -6,5 +6,9 @@ namespace Penguin\ConsoleApp;
 
 class Commands
 {
-
+    public function scan()
+    {
+        $config = json_decode(file_get_contents(__DIR__ . '/../config.json'), true);
+        $files = Helpers::getFiles(__DIR__ . '/../' . $config['folder']);
+    }
 }
